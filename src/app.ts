57b75e1d1bb30c66e7394ddbbbe6c1a2f1ba33c0
shofikul <179398ssshofik@gmail.com>
2024-02-20 12:45:51 +0600

    add custom middleware for resolving cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,28 @@
 import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
+
 import router from './app/routes';
 import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/gobalErrorHandler';
 const app = express();
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Adjust the origin based on your client's URL
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 //parser
-// app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use(
-  cors({
-    origin: 'https://whats-app-clone-frontend-pi.vercel.app',
-    credentials: true,
-  })
-);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use('/api/v1', router);
-app.use(globalErrorHandler);
 
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'WhatsApp application server running successfully',
   });
 });
+app.use('/api/v1', router);
+app.use(globalErrorHandler);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
