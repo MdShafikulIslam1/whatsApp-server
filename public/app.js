d321c351fa4ba17f889fdc349ef6b0bdc4dfbb2c
shofikul <179398ssshofik@gmail.com>
2024-02-20 13:05:19 +0600

    add new version of cors

--- public/app.js
+++ public/app.js
@@ -5,28 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./app/routes"));
+const cors_1 = __importDefault(require("cors"));
 const http_status_1 = __importDefault(require("http-status"));
 const gobalErrorHandler_1 = __importDefault(require("./app/middlewares/gobalErrorHandler"));
-const config_1 = __importDefault(require("./config"));
+// import config from './config';
 const app = (0, express_1.default)();
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', config_1.default.access_frontend_url ||
-        'https://whats-app-clone-frontend-pi.vercel.app'); // Adjust the origin based on your client's URL
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 //parser
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use('/api/v1', routes_1.default);
+app.use(gobalErrorHandler_1.default);
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'WhatsApp application server running successfully',
     });
 });
-app.use('/api/v1', routes_1.default);
-app.use(gobalErrorHandler_1.default);
 app.use((req, res, next) => {
     res.status(http_status_1.default.NOT_FOUND).json({
         success: false,
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,24 @@
 import express, { NextFunction, Request, Response } from 'express';
-
 import router from './app/routes';
+import cors from 'cors';
 import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/gobalErrorHandler';
-import config from './config';
+// import config from './config';
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    config.access_frontend_url ||
-      'https://whats-app-clone-frontend-pi.vercel.app'
-  ); // Adjust the origin based on your client's URL
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-//parser
 
+//parser
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', router);
+app.use(globalErrorHandler);
 
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'WhatsApp application server running successfully',
   });
 });
-app.use('/api/v1', router);
-app.use(globalErrorHandler);
-
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
